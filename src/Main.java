bac66f3e0d50415ff2483f9161fb9d489bcee651
Алексей Порфиров <alex.porfirov@gmail.com>
2022-08-16 20:34:48 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        int beforbalance = 100;
-        int popolnenie = 1500;
-        int bonus = beforbalance + popolnenie / 100;
-        if (popolnenie < 1000)
+        int beforereplenishment = 100;
+        int replenishment = 1500;
+        int bonus = beforereplenishment + replenishment / 100;
+        if (replenishment < 1000) {
             bonus = 0;
-        int total = beforbalance + popolnenie + bonus;
+        }
+        int total = beforereplenishment + replenishment + bonus;
         System.out.println("Кол-во бонусов: " + bonus);
         System.out.println("Итоговый баланс: " + total);
     }
