013f407b4a7a6c341d9879fadf84462f70be7e72
Алексей Порфиров <alex.porfirov@gmail.com>
2022-08-21 12:41:12 +0300

    again

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        int beforereplenishment = 100;
-        int replenishment = 1500;
-        int bonus = beforereplenishment + replenishment / 100;
-        if (replenishment < 1000) {
-            bonus = 0;
+        int BeforeReplenishment = 100;
+        int Replenishment = 1500;
+        int Bonus = BeforeReplenishment + Replenishment / 100;
+        if (Replenishment < 1000) {
+            Bonus = 0;
         }
-        int total = beforereplenishment + replenishment + bonus;
-        System.out.println("Кол-во бонусов: " + bonus);
-        System.out.println("Итоговый баланс: " + total);
+        int Total = BeforeReplenishment + Replenishment + Bonus;
+        System.out.println("Кол-во бонусов: " + Bonus);
+        System.out.println("Итоговый баланс: " + Total);
     }
     }
